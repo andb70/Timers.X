85ae3c7804b52f6e552d5232079b92d22fe5cd89
andb70 <andb70@gmail.com>
2019-07-08 12:24:12 +0200

    Beta release

---
+++ main.c
@@ -0,0 +1,239 @@
+/*
+ *      Author:     Andrea Biasutti
+ *      Date:       July 5th 2019
+ * 
+ *      Subject:    TSIOT - Interconnessione di DEVICE
+ * 
+ */
+#pragma config FPLLIDIV = DIV_2 // PLL Input Divider (1x Divider)
+#pragma config FPLLMUL = MUL_20 // PLL Multiplier (24x Multiplier)
+#pragma config UPLLIDIV = DIV_2 // USB PLL Input Divider (12x Divider)
+#pragma config UPLLEN = OFF // USB PLL Enable (Disabled and Bypassed)
+#pragma config FPLLODIV = DIV_1 // System PLL Output Clock Divider (PLL Divide by 256)
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
+#pragma config FSOSCEN = ON // Secondary Oscillator Enable (Enabled)
+#pragma config IESO = ON // Internal/External Switch Over (Enabled)
+#pragma config POSCMOD = HS // Primary Oscillator Configuration (HS osc mode)
+#pragma config OSCIOFNC = ON // CLKO Output Signal Active on the OSCO Pin (Enabled)
+#pragma config FPBDIV = DIV_8 // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/8)
+#pragma config FCKSM = CSDCMD // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
+#pragma config WDTPS = PS1048576 // Watchdog Timer Postscaler (1:1048576)
+#pragma config FWDTEN = OFF // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
+// DEVCFG0
+#pragma config DEBUG = OFF // Background Debugger Enable (Debugger is disabled)
+#pragma config ICESEL = ICS_PGx2 // ICE/ICD Comm Channel Select (ICE EMUC2/EMUD2 pins shared with PGC2/PGD2)
+#pragma config PWP = OFF // Program Flash Write Protect (Disable)
+#pragma config BWP = OFF // Boot Flash Write Protect bit (Protection Disabled)
+#pragma config CP = OFF // Code Protect (Protection Disabled)
+
+#include <p32xxxx.h> // Include PIC32 specifics header file
+#include <plib.h> // Include the PIC32 Peripheral Library
+#include "timers.h"
+
+// use the green led to monitor hertbeat
+#define monitorGLed(state)      LATGbits.LATG6 = state
+#define monitorGLedToggle()     LATGbits.LATG6 = !LATGbits.LATG6
+
+// use the yellow led to monitor timer_us(0)
+#define monitorYLed(state)      LATDbits.LATD1 = state
+#define monitorYLedToggle()     LATDbits.LATD1 = !LATDbits.LATD1
+
+// use the red led to monitor timer_us(x)
+#define monitorRLed(state)      LATBbits.LATB13 = state
+#define monitorRLedToggle()     LATBbits.LATB13 = !LATBbits.LATB13
+/******************************************************************************/
+int main() {
+
+    // enable leds
+    PORTSetPinsDigitalOut(IOPORT_D, BIT_1); //  YELLOW led on-board
+    PORTSetPinsDigitalOut(IOPORT_G, BIT_6); //  GREEN  led on-board
+    mJTAGPortEnable(0); // Disable JTAG
+    PORTSetPinsDigitalOut(IOPORT_B, BIT_13); // RED led @ D8  
+    
+    // switch all off
+    LATD=0;
+    LATB=0;
+    LATG=0;
+    
+    // initialize timer service
+    initTimers();  
+    
+    unsigned int count = 0;
+    while (1) 
+    {
+        // read input
+        checkTimers();
+
+        if(heartbeat()) 
+        {
+            monitorYLedToggle();
+        }
+
+        if (timer1Status())
+        {
+            if (++count == TMR_FREQ_1)
+            {
+                count = 0;
+                monitorYLedToggle();
+            }
+        }
+        if(timer_usElapsed(0))
+        {
+            monitorGLedToggle();
+            timer_usSet(0, TMR_FREQ_2<<TMR_FREQ_K);
+        }
+        if(timer_usElapsed(1))
+        {
+            monitorRLedToggle();
+            timer_sSet(1, 1);
+        }
+        
+        /**********************************************************************/
+        // test full instance impact
+        if(timer_usElapsed(2))
+        {
+            monitorRLedToggle();
+            timer_sSet(2, 1);
+        }
+        if(timer_usElapsed(3))
+        {
+            monitorRLedToggle();
+            timer_sSet(3, 1);
+        }
+        if(timer_usElapsed(4))
+        {
+            monitorRLedToggle();
+            timer_sSet(4, 1);
+        }
+        if(timer_usElapsed(5))
+        {
+            monitorRLedToggle();
+            timer_sSet(5, 1);
+        }
+        if(timer_usElapsed(6))
+        {
+            monitorRLedToggle();
+            timer_sSet(6, 1);
+        }
+        if(timer_usElapsed(7))
+        {
+            monitorRLedToggle();
+            timer_sSet(7, 1);
+        }
+        if(timer_usElapsed(8))
+        {
+            monitorRLedToggle();
+            timer_sSet(8, 1);
+        }
+        if(timer_usElapsed(9))
+        {
+            monitorRLedToggle();
+            timer_sSet(9, 1);
+        }
+        if(timer_usElapsed(10))
+        {
+            monitorRLedToggle();
+            timer_sSet(10, 1);
+        }
+        if(timer_usElapsed(11))
+        {
+            monitorRLedToggle();
+            timer_sSet(11, 1);
+        }
+        if(timer_usElapsed(12))
+        {
+            monitorRLedToggle();
+            timer_sSet(12, 1);
+        }
+        if(timer_usElapsed(13))
+        {
+            monitorRLedToggle();
+            timer_sSet(13, 1);
+        }
+        if(timer_usElapsed(14))
+        {
+            monitorRLedToggle();
+            timer_sSet(14, 1);
+        }
+        if(timer_usElapsed(15))
+        {
+            monitorRLedToggle();
+            timer_sSet(15, 1);
+        }
+        /*if(timer_usElapsed(16))
+        {
+            monitorRLedToggle();
+            timer_sSet(16, 1);
+        }
+        if(timer_usElapsed(17))
+        {
+            monitorRLedToggle();
+            timer_sSet(17, 1);
+        }
+        if(timer_usElapsed(18))
+        {
+            monitorRLedToggle();
+            timer_sSet(18, 1);
+        }
+        if(timer_usElapsed(19))
+        {
+            monitorRLedToggle();
+            timer_sSet(19, 1);
+        }
+        if(timer_usElapsed(20))
+        {
+            monitorRLedToggle();
+            timer_sSet(20, 1);
+        }
+        if(timer_usElapsed(21))
+        {
+            monitorRLedToggle();
+            timer_sSet(21, 1);
+        }
+        if(timer_usElapsed(22))
+        {
+            monitorRLedToggle();
+            timer_sSet(22, 1);
+        }
+        if(timer_usElapsed(23))
+        {
+            monitorRLedToggle();
+            timer_sSet(23, 1);
+        }
+        if(timer_usElapsed(24))
+        {
+            monitorRLedToggle();
+            timer_sSet(24, 1);
+        }
+        if(timer_usElapsed(25))
+        {
+            monitorRLedToggle();
+            timer_sSet(25, 1);
+        }
+        if(timer_usElapsed(26))
+        {
+            monitorRLedToggle();
+            timer_sSet(26, 1);
+        }
+        if(timer_usElapsed(27))
+        {
+            monitorRLedToggle();
+            timer_sSet(27, 1);
+        }
+        if(timer_usElapsed(28))
+        {
+            monitorRLedToggle();
+            timer_sSet(28, 1);
+        }
+        if(timer_usElapsed(29))
+        {
+            monitorRLedToggle();
+            timer_sSet(29, 1);
+        }*/
+
+        /**********************************************************************/
+        
+    }
+    return 1;
+}
---
+++ timers.c
@@ -0,0 +1,98 @@
+#include <p32xxxx.h> // Include PIC32 specifics header file
+#include <plib.h> // Include the PIC32 Peripheral Library
+#include "timers.h"
+
+union _timerElapsed {
+    struct {
+        unsigned timer1 : 1;
+        unsigned timer2 : 1;
+        unsigned : 6;
+    };
+    struct {
+        unsigned w : 8;
+    };
+} timerNotify;
+
+// heartbeat base pulsation is under timer2 scan frequency, hence in us
+unsigned int heartbeatCount;
+
+void initTimers() {
+    // enable multi-vector interrupts
+    INTEnableSystemMultiVectoredInt();
+    // optimize pic32 performances
+    SYSTEMConfigPerformance(SYS_FREQ);
+
+    // configure Timer 1 using internal clock
+    OpenTimer1(T1_EN | T1_SOURCE | PRESCALE_1, PR_1);
+    // set up the timer interrupt with a priority of 2
+    ConfigIntTimer1(T1_INT | T1_INT_PRIOR);
+
+    // configure Timer 1 using internal clock
+    OpenTimer2(T2_ON | T2_SOURCE_INT | PRESCALE_2, PR_2);
+    // set up the timer interrupt with a priority of 2
+    ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2);
+    
+    // reset the flags and counter
+    heartbeatCount = TMR_FREQ_2;
+    timerNotify.w=0;
+}
+
+void __ISR(_TIMER_1_VECTOR, IPL2SOFT) Timer1Handler(void) {
+    // set the notify bit
+    timerNotify.timer1 = 1;
+    // clear the interrupt flag
+    mT1ClearIntFlag();
+}
+void __ISR(_TIMER_2_VECTOR, IPL2SOFT) Timer2Handler(void) {
+    // set the notify bit
+    timerNotify.timer2 = 1;
+    // clear the interrupt flag
+    mT2ClearIntFlag();
+}
+
+void checkTimers() {
+    // sync the timer status to be available without change for the entire cycle
+
+    timerFlags.timer1=0;
+    if ( timerNotify.timer1)
+    {
+        timerNotify.timer1 = 0;
+        timerFlags.timer1=1;
+    }
+    
+    // reset the heartbeat
+    timerFlags.hertbeat = 0;
+    timerFlags.timer2=0;
+    if (timerNotify.timer2)
+    {
+        timerNotify.timer2 = 0;
+        timerFlags.timer2=1;
+
+        if (--heartbeatCount == 0) {
+            timerFlags.hertbeat = 1;
+            heartbeatCount = TMR_FREQ_2;
+        }
+
+        // compute all the timers
+        char i;
+        for (i=0; i< NUM_TIMERS_us; i++)
+        {
+            // if the timer is counting
+            if (timerFlags.w>>i & 0x01)
+            {        
+                // decrement and when reach 0 stop counting
+                 if(--timerIntervals[i]==0)                
+                    timerFlags.w &= ~(1<<i);            
+            }
+        }
+    }
+
+}
+
+
+void timer_usSet(char id, unsigned int interval){
+    // set the status bit
+    timerFlags.w |= 1<<id;
+    // reset the interval
+    timerIntervals[id] = (interval>>TMR_FREQ_K);
+}
